4debe5cb396fce9ccae0043554600cd63ba427ba
Mick Vleeshouwer <mick@imick.nl>
2018-03-17 20:25:28 +0100

    Add alert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { OpenALPR, OpenALPROptions, OpenALPRResult } from '@ionic-native/openalpr';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,7 @@ import { OpenALPR, OpenALPROptions, OpenALPRResult } from '@ionic-native/openalp
 })
 export class HomePage {
 
-  constructor(private camera: Camera, private openALPR: OpenALPR) {
+  constructor(private camera: Camera, private openALPR: OpenALPR, private alertCtrl: AlertController) {
 
   }
 
@@ -36,11 +37,37 @@ export class HomePage {
       .then((imageData) => {
         this.openALPR.scan(imageData, scanOptions)
           .then((result: [OpenALPRResult]) => {
-            const bestResult = result[0];
 
-            console.log(bestResult.confidence);
-            console.log(bestResult.number);
+            if (result.length < 1) {
+              this.showError('No license plates found');
+              return;
+            }
+
+            const bestResult = result[0];
+            this.showResult(bestResult);
+
           }).catch((error: Error) => console.error(error));
       }).catch((error: Error) => console.error(error));
+
+  }
+
+  showError(text: string) {
+    const alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: text,
+      buttons: ['Ok!']
+    });
+
+    alert.present();
+  }
+
+  showResult(result: OpenALPRResult) {
+    const alert = this.alertCtrl.create({
+      title: 'Scan complete',
+      message: `${result.number} (${result.confidence})`,
+      buttons: ['Ok!']
+    });
+
+    alert.present();
   }
 }
