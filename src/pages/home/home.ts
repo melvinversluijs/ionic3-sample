9fca9192f79c6f8a302b120ef5b3c726122e65a5
Mick Vleeshouwer <mick@imick.nl>
2018-03-17 17:17:11 +0100

    Make picture source optional

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,30 +13,41 @@ export class HomePage {
 
   }
 
-  scan() {
+  scan(input: string) {
+
+    let type = this.camera.PictureSourceType.PHOTOLIBRARY;
+
+    if (input == 'camera') {
+      type = this.camera.PictureSourceType.CAMERA;
+    }
+
     if (cordova !== undefined) {
+
       const cameraOptions: CameraOptions = {
         quality: 100,
         destinationType: this.camera.DestinationType.FILE_URI,
         encodingType: this.camera.EncodingType.JPEG,
         mediaType: this.camera.MediaType.PICTURE,
-        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+        sourceType: type,
         allowEdit: false
       }
 
       this.camera.getPicture(cameraOptions).then((imageData) => {
+
         cordova.plugins.OpenALPR.scan(
-          imageData, 
-          (response) => { 
+          imageData,
+          (response) => {
             console.log(response)
-          }, 
-          (error) => { 
+          },
+          (error) => {
             console.log(error)
           }
         );
+
       }, (error) => {
         console.log(error);
       });
+
     } else {
       console.log('fail');
     }
